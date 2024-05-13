9de458e9878eba6879cfed9d2b80ef3486d4ab88
Tannpopo Guan <tannpopo0116@gmail.com>
2024-05-13 20:47:33 +0900

    fix bug and add test and add webcamera

---
+++ camera/webcamera.py
@@ -0,0 +1,25 @@
+import cv2
+
+
+# url = "http://admin:admin@192.168.3.11:8081"
+# 程序是执行状态，但是没有打开摄像头
+# url = "http://admin:123456@192.168.3.16:8081"
+# 正确打开摄像头
+
+url = "rtsp://admin:admin@192.168.3.16:8554/live"
+# 直接返回错误：[rtsp @ 000001ee2b0824c0] method DESCRIBE failed: 401 Unauthorized
+
+# url = "rtsp://admin:123456@192.168.3.16:8554/live"
+# [rtsp @ 0000019250a424c0] method DESCRIBE failed: 404 Stream Not Found  手机的摄像头会被打开，然后程序报错
+
+print('start')
+cap = cv2.VideoCapture(url)#读取视频流
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    print('success')
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python people_counter.py --prototxt detector/MobileNetSSD_deploy.prototxt --model detector/MobileNetSSD_deploy.caffemodel --input utils/data/tests/test_2.mp4
